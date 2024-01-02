f5dec5f3de11562611259bbc3268441cb6780545
Alexander Musonda <74776587+musondaAlexander@users.noreply.github.com>
2024-01-02 20:42:43 +0200

    updated authentication UI

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'theme/colors.dart';
 import 'package:flutter/material.dart';
 import 'pages/home.dart';
+import 'package:telemed/authentication/ui/Screens/Welcome/welcome_screen.dart';
+import 'package:telemed/authentication/ui/constants.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,12 +12,34 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primaryColor: primary,
-      ),
-      home: Home(),
-    );
+        debugShowCheckedModeBanner: false,
+        title: 'Telemed',
+        theme: ThemeData(
+            primaryColor: kPrimaryColor,
+            scaffoldBackgroundColor: Colors.white,
+            elevatedButtonTheme: ElevatedButtonThemeData(
+              style: ElevatedButton.styleFrom(
+                elevation: 0,
+                foregroundColor: Colors.white,
+                backgroundColor: kPrimaryColor,
+                shape: const StadiumBorder(),
+                maximumSize: const Size(double.infinity, 56),
+                minimumSize: const Size(double.infinity, 56),
+              ),
+            ),
+            inputDecorationTheme: const InputDecorationTheme(
+              filled: true,
+              fillColor: kPrimaryLightColor,
+              iconColor: kPrimaryColor,
+              prefixIconColor: kPrimaryColor,
+              contentPadding: EdgeInsets.symmetric(
+                  horizontal: defaultPadding, vertical: defaultPadding),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(30)),
+                borderSide: BorderSide.none,
+              ),
+            )),
+        // home: Home(),
+        home: const WelcomeScreen());
   }
 }
