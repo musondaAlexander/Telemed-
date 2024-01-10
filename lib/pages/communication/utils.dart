7f52a3af547faab92bd9c73b8e9a33a04f690f69
Alexander Musonda <74776587+musondaAlexander@users.noreply.github.com>
2024-01-10 12:11:36 +0200

    updated dependacies

---
+++ lib/pages/communication/utils.dart
@@ -0,0 +1,10 @@
+class Utils {
+// variables for Video Calls
+  static int AppIDVideoCall = 509965091;
+  static String AppSignVideoCall =
+      "85937de54c8af50467e57f03e7ac5edf26184da006b85e4a9566261256c2c2a9";
+  // variables for In-App Messaging
+  static int AppIDChat = 564745319;
+  static String AppSignChat =
+      "aba802fa0b07b760e393e6999944ea50af661b7cda04c301e6e8247d599f6960";
+}
